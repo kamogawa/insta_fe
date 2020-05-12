688e23d2cbc203340cb26fed506761836e5b86c6
BEUMGOO <captain_kbg@naver.com>
2020-05-12 23:43:21 +0900

    feed/loader component

--- src/Routes/Feed.js
+++ src/Routes/Feed.js
@@ -1,1 +1,21 @@
-export default () => "Feed";
+import React from "react";
+import styled from "styled-components";
+import { gql } from "apollo-boost";
+import { useQuery } from "react-apollo-hooks";
+import Loader from "../Components/Loader";
+
+const FEED_QEERY = gql`
+
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 80vh;
+`;
+
+export default () => {
+  const { data, loading } = useQuery(FEED_QEERY);
+  return <Wrapper>{loading && <Loader />}</Wrapper>;
+}
