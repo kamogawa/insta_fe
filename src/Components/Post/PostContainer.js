e858e98a5ee069270e842866365feb11737dc2f2
BEUMGOO <captain_kbg@naver.com>
2020-05-15 18:53:38 +0900

    post component 02

--- src/Components/Post/PostContainer.js
+++ src/Components/Post/PostContainer.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import PostPresenter from "./PostPresenter";
 import useInput from "../../Hooks/useInput";
@@ -16,7 +16,17 @@ const PostContainer = ({
 }) => {
   const [isLikedS, setIsLiked] = useState(isLiked);
   const [likeCountS, setLikeCount] = useState(likeCount);
+  const [currentItem, setCurrentItem] = useState(0);
   const comment = useInput("");
+
+  useEffect(() => {
+    const totalFiles = files.length;
+    if (currentItem === totalFiles - 1) {
+      setTimeout(() => setCurrentItem(0), 2000);
+    } else {
+      setTimeout(() => setCurrentItem(currentItem + 1), 2000);
+    }
+  }, [currentItem, files]);
 
   return (
     <PostPresenter 
@@ -31,6 +41,7 @@ const PostContainer = ({
       newComment={comment}
       setIsLiked={setIsLiked}
       setLikeCount={setLikeCount}
+      currentItem={currentItem}
     />
   );
 };
